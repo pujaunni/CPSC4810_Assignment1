7a3582f07afd6ce17df8a21cb542496584379c9c
Puja <pujaunni2009@gmail.com>
2020-06-05 15:24:51 -0700

    Added my name at the end of all files

--- q1_b.sh
+++ q1_b.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo "Top 3 Destination Airports with Arriving Flight Count"
 csvcut -c FlightNum,Dest flightdelay2007.csv | csvstat -c Dest --freq-count 3 | csvlook
+echo "Puja Unni"
