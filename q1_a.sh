cd0373944546fccf5160b3ace60a646f9d86c604
Puja <pujaunni2009@gmail.com>
2020-06-08 22:53:01 -0700

    Updated Q1_b logic

--- q1_a.sh
+++ q1_a.sh
@@ -2,4 +2,3 @@
 echo "Output is "
 csvgrep -c Origin -m "SFO" flightdelay2007.csv | csvcut -c ArrDelay,Origin | csvlook | head -5 > first3sfo.csv
 cat first3sfo.csv
-echo "Puja Unni"
--- q1_b.sh
+++ q1_b.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 echo "Top 3 Destination Airports with Arriving Flight Count"
-csvcut -c FlightNum,Dest flightdelay2007.csv | csvstat -c Dest --freq-count 3 | csvlook
-echo "Puja Unni"
+csvcut -c Dest flightdelay2007.csv | sort | uniq -c | sort -r -n | head -3 | csvlook -H -y 0
